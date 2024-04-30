e57a12276d47cdf0da3d40e6b04c067e14f0620a
prismtechbd3 <prismtechb@gmail.com>
2024-04-30 22:01:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+=> instalation
+=> Admin control
+-> php artisan make:middleware AdminMiddleware
+=> flowbite css 
+
+npm install flowbite
+in tailwind config 
+=> plugins
+ require('flowbite/plugin')
+ => content
+"./node_modules/flowbite/**/*.js"
+
+
+
+
+php artisan optimize
+php artisan config:clear
---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+       
+        
+        <!-- Navbar -->
+              <Navbar/>
+        <!-- Endnavbar -->
+   
+              <Sidebar/>
+        <!-- Sidebar -->
+
+        <!-- EndSidebar -->
+
+          <slot/>
+       
+       <!-- Footer -->
+
+       
+        
+      </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+
+</script>
