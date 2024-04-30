66576eaa4f6fa4fb61f8232743693462ca913063
prismtechbd3 <prismtechb@gmail.com>
2024-05-01 05:56:19 +0600

    next

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -25,7 +25,6 @@ import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 import Navbar from './Navbar.vue'
 import Sidebar from './Sidebar.vue'
-// initialize components based on data attribute selectors
 onMounted(() => {
     initFlowbite();
 })
